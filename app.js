ab3ebc55e41500a8baedf7deb12ff5758be164e4
Brittany <bgreensl@ualberta.ca>
2021-04-08 21:57:34 -0600

    finished dropdown menu requests/fns

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function randomActFailure(err) {
     "activityContainer"
   ).innerHTML = `<h3>Something went wrong! Please try again</h3>`;
 }
-function getActivity() {
+function newActivity() {
   axios
     .request({
       method: "GET",
@@ -22,4 +22,35 @@ function getActivity() {
     .catch(randomActFailure);
 }
 let newActBtn = document.getElementById("newActBtn");
-newActBtn.addEventListener("click", getActivity);
+newActBtn.addEventListener("click", newActivity);
+
+function showActSuccess(res) {
+  let activities = res.data;
+  let activityContainer = document.getElementById("activityContainer");
+  let activity = activities.activity;
+  let participants = activities.participants;
+  activityContainer.innerHTML = `<h2>Activity with ${participants} participants: ${activity}</h2>`;
+}
+
+function showActError(err) {
+  document.getElementById(
+    "activityContainer"
+  ).innerHTML = `<h3>Something went wrong! Please try again</h3>`;
+}
+function showActivity() {
+  let participantNum = document.getElementById("participants").value;
+  axios
+    .request({
+      method: "GET",
+      url: `http://www.boredapi.com/api/activity`,
+      headers: { "Content-Type": "application/json" },
+      params: {
+        participants: `${participantNum}`,
+      },
+    })
+    .then(showActSuccess)
+    .catch(showActError);
+}
+
+let showActBtn = document.getElementById("showActBtn");
+showActBtn.addEventListener("click", showActivity);
