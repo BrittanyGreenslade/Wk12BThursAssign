883c098d6357fd4496f324d56b9cab8f37346667
Brittany <bgreensl@ualberta.ca>
2021-04-09 01:42:40 -0600

    finished bonus 1 for categories with radio buttons

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//first button
 function randomActSuccess(res) {
   let activities = res.data;
   let activityContainer = document.getElementById("activityContainer");
@@ -12,6 +13,7 @@ function randomActFailure(err) {
     "activityContainer"
   ).innerHTML = `<h3>Something went wrong! Please try again</h3>`;
 }
+//second button
 function newActivity() {
   axios
     .request({
@@ -54,3 +56,36 @@ function showActivity() {
 
 let showActBtn = document.getElementById("showActBtn");
 showActBtn.addEventListener("click", showActivity);
+//bonus 1
+function selectTypeSuccess(res) {
+  let activities = res.data;
+  let activityContainer = document.getElementById("activityContainer");
+  let activity = activities.activity;
+  let type = activities.type;
+  activityContainer.innerHTML = `<h2>An activity in category ${type} is: ${activity}</h2>`;
+}
+function selectTypeError(err) {
+  console.log(err);
+}
+
+function selectType() {
+  let activityType = document.querySelector(`input:checked`).value;
+  axios
+    .request({
+      method: "GET",
+      url: `http://www.boredapi.com/api/activity`,
+      headers: { "Content-Type": "application/json" },
+      params: {
+        type: `${activityType}`,
+      },
+    })
+    .then(selectTypeSuccess)
+    .catch(selectTypeError);
+  // console.log(selectedActivity);
+}
+let submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", selectType);
+// console.log(selectedActivity);
+//Wk12BThursAssign
+//this documentation allows you to send parametes to filter what activites are sent
+//back to you (type, price range, min/max price, )
